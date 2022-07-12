a18e82a4235ac1a25b27e496f355554cdcef23ba
Lucy de Rojas <lucyderojas@gmail.com>
2022-07-12 14:51:16 +0100

    added comps and media

---
+++ Components/Navigation.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+export default function Navigation() {
+
+
+
+
+
+
+    return (<div id='navigation'>
+
+        <h1>navigation</h1>
+
+        <ul>
+
+            <li>About</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+        </ul>
+
+
+
+
+
+    </div>);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
