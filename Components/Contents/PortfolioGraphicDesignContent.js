3318c3a08b74e36fb69a75f55714d60ecda02105
Lucy de Rojas <lucyderojas@gmail.com>
2022-08-02 08:16:08 +0100

    fdsf

--- Components/Contents/PortfolioGraphicDesignContent.js
+++ Components/Contents/PortfolioGraphicDesignContent.js
@@ -59,6 +59,13 @@ Magazine:
         
         
         /> 
+
+
+
+
+        from old site:
+        <iframe src="https://onedrive.live.com/embed?resid=EC6BF2592866F3BD%21133&amp;authkey=%21AB0z1gC--kIE3K4&amp;em=2&amp;wdAr=1.7777777777777777&amp;Embed=1&amp;wdEaa=1" width="100%" height="auto" frameBorder="1">This is an embedded <a href="https://office.com">Microsoft Office</a> presentation, powered by <a href="https://office.com/webapps">Office</a>.</iframe>
+
         
         
         
