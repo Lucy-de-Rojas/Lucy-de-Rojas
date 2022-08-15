b735952b9586f824940ad30c5cd55c66f08f9da7
Lucy de Rojas <lucyderojas@gmail.com>
2022-08-15 08:35:06 +0100

    step about

--- Components/Contents/aboutContent.js
+++ Components/Contents/aboutContent.js
@@ -12,6 +12,10 @@ export default function AboutContent() {
     return (<LayoutContent>
 
 <h1>About</h1>
+<p>Lucy is an active, techie mum and wife who loves technology and art. With professional sporting
+past in athletics, her work style carries strong rule adhering, self-discipline and bringing projects to
+successful finish. With background in graphic design she wants to add full stack web developement
+into her diverse skills toolbox.</p>
 
 
 
