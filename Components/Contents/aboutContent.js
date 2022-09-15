9afa28a232c25cd15688c0b0022884bd247d1957
Lucy de Rojas <lucyderojas@gmail.com>
2022-09-15 13:30:11 +0100

    added about image

--- Components/Contents/aboutContent.js
+++ Components/Contents/aboutContent.js
@@ -1,5 +1,5 @@
-
-
+import Image from "next/image";
+import styles from '../../styles/about.module.css';
 
 
 
@@ -11,11 +11,33 @@ export default function AboutContent() {
 
     return (<LayoutContent>
 
+
+<div className={styles.image}>
+
+        <Image 
+        
+        src= "/Media/LucyDeRojas.jfif"
+        width={500}
+        height={500}
+        layout="responsive"
+        alt="Lucy de Rojas photo"
+        className={styles.image}
+        
+        /></div>
+
 <h1>About</h1>
 <p>Lucy is an active, techie mum and wife who loves technology and art. With professional sporting
 past in athletics, her work style carries strong rule adhering, self-discipline and bringing projects to
-successful finish. With background in graphic design she wants to add full stack web developement
-into her diverse skills toolbox.</p>
+successful finish. After completing  
+
+
+With background in graphic design she wants to add full stack web developement
+into her diverse skills toolbox.
+
+
+
+
+</p>
 
 
 
