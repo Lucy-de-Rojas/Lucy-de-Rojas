76c2194f4655cf36d178600cf397c0888f5243fa
Lucy de Rojas <lucyderojas@gmail.com>
2022-09-22 08:33:42 +0100

    added technologies to About page

--- Components/Contents/aboutContent.js
+++ Components/Contents/aboutContent.js
@@ -14,13 +14,13 @@ let allIcons = [
     },
     {
         href: '/Media/IconsAndLogos/Cypress.png',
-        with: 403,
+        width: 403,
         height: 135,
         alt: 'Cypress Testing Logo'
 
     }, {
         href: '/Media/IconsAndLogos/figma.png',
-        with:999 ,
+        width:999 ,
         height:478 ,
         alt: 'Figma Logo'
     }, {
@@ -30,9 +30,44 @@ let allIcons = [
         alt: 'Heroku Logo'
     }, {
         href: '/Media/IconsAndLogos/jest.png',
-        with: 296,
+        width: 296,
         height: 142,
         alt: 'Jest Testing logo'
+    }, {
+        href: '/Media/IconsAndLogos/Nextjs-Logo.png',
+        width: 800,
+        height: 479,
+        alt:'NextJs Logo'
+    }, {
+        href: '/Media/IconsAndLogos/PostgreSQL-Logo.png',
+        width: 799,
+        height: 296,
+        alt: 'PostgreSQL Logo'
+    }, {
+        href: '/Media/IconsAndLogos/python-logo.png',
+        width: 476,
+        height: 132,
+        alt: 'Python logo'
+    }, {
+        href: '/Media/IconsAndLogos/React_logo.png',
+        width: 5000,
+        height:1679 ,
+        alt: 'React logo'
+    }, {
+        href: '/Media/IconsAndLogos/scratch.png',
+        width: 815,
+        height: 625,
+        alt: 'Scratch logo'
+    }, {
+        href: '/Media/IconsAndLogos/Trello_logo.png',
+        width: 1280,
+        height: 366,
+        alt: 'Trello Logo'
+    }, {
+        href: '/Media/IconsAndLogos/vsCode.png',
+        width: 637,
+        height: 179,
+        alt: 'VS Code Logo'
     }
 
 ]
@@ -89,15 +124,13 @@ With background in graphic design she graduated from School of Code as a full st
     /> */}
 
 
-
-
-
+    <h2 className={styles.headerTechnologies}>Favourite techologies and tools:</h2>
             {
               
               allIcons.map((item)=> {
-                return (<div className={styles.iconImage}>
+                return (
                     <img src={item.href} width={item.width} height={item.height} className={styles.iconImage}/>
-                </div>);
+                );
               })
 
             }
