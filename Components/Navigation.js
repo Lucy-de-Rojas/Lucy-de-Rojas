1b2caff24304eca08f42acddf00ab62955bd19ce
Lucy de Rojas <lucyderojas@gmail.com>
2022-11-08 09:24:05 +0000

    NAVIGATION -> changing clicked buttons background colours to red

--- Components/Navigation.js
+++ Components/Navigation.js
@@ -7,8 +7,25 @@ import styles from '../styles/navigation.module.css';
 
 
 
-
 export default function Navigation() {
+
+
+
+    const handleClick = (event)=> {
+        console.clear();
+
+
+        event.target.style.backgroundColor = 'rgb(255, 0, 0)';
+
+
+
+
+        console.log('clicked:::>', event.target.innerHTML);
+
+    }
+
+
+
 
 
 
@@ -22,19 +39,19 @@ export default function Navigation() {
 
     return (<div className={styles.navigation}>
 
-            <Link href="/about" >
-            <a className={styles.nav}> About</a>
+            <Link href="/about"  >
+            <a className={styles.nav} id={styles.about}  onClick={handleClick}> About</a>
              </Link>
 
 
-             <Link href="/portfolio">
-             <a className={styles.nav}>Portfolio </a>
+             <Link href="/portfolio" >
+             <a className={styles.nav} onClick={handleClick}>Portfolio </a>
              </Link>
 
 
 
-             <Link href="/contact">
-             <a className={styles.nav}>Contact</a>
+             <Link href="/contact" >
+             <a className={styles.nav} onClick={handleClick}>Contact</a>
              </Link>
 
 
