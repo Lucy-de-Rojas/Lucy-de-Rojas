97f72232fbb68754c5dcafc5da9e7b51886d5ab4
Lucy de Rojas <lucyderojas@gmail.com>
2023-02-19 14:15:38 +0000

    web portfolio added recommendation section

--- Components/Contents/PortfolioContent.js
+++ Components/Contents/PortfolioContent.js
@@ -1,8 +1,17 @@
 
+import Image from "next/image";
+import Link from "next/link";
 
 
 
 import LayoutContent from "./LayoutContent";
+import styles from '../../styles/portfolioContent.module.css';
+
+
+
+
+
+
 
 
 
@@ -12,14 +21,43 @@ export default function PortfolioContent() {
 
     return (<LayoutContent>
 
-        <h1>Portfolios</h1>
-
-        <p>
-            This Portfolio consist of web development and graphic design sections.
+        <h1>Best of Both </h1>
 
 
 
+{/* BEST OF BOTH:   */}
+<div className={styles.imageWrapper}>
+
+        <Image
+            src="/Media/Photos/BestOfBoth.jpg"
+            width={900}
+            height={671}
+            alt="best of both photo of hovis bread"
+            layout="responsive"
+
+
+            />
+            </div>
+
+
+
+
+
+            <p>
+In this section I have decided to take 3 top picks from both worlds, from print design and web development.
         </p>
+
+
+
+<h2>1. Menu13</h2>
+
+<h2>2. Laceys website</h2>
+<h2>3. Centenary celebration video</h2>
+
+
+
+
+
     </LayoutContent>);
 }
 
