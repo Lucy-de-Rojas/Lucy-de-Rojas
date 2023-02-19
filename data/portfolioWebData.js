f16c91c8d23095b55cbe94e6c10320deaa88fa51
Lucy de Rojas <lucyderojas@gmail.com>
2023-02-19 13:01:43 +0000

    added more web portfolio content

--- data/portfolioWebData.js
+++ data/portfolioWebData.js
@@ -1,12 +1,15 @@
 export const portfolioWebData = [
+
+
     // NAPHILL TENNIS CLUB:
     {
         header: "Naphill Tennis Club",
-        description: "About the site ",
+        description: "Naphill Tennis Club website has been redesigned to a new fresh look, with new logo and colour theme. It includes messages being saved to a database and message board to see all the messages. There was more visual content made in form of video introduction and posters advertising upcoming events. SEO of the site improved search results for anyone seeking to play tennis in Buckinghamshire area.",
         src:"/Media/screenshots/naphill-tennis-club-site.jpg",
         quality: 100,
         width: 900,
         height: 746,
+        recommendation: "recommendation from naphill",
         alt: "screenshot Trivia website",
         githubLink: "https://github.com/Lucy-de-Rojas/naphill-tennis-club",
         liveSiteLink: 'https://naphill-tennis-club.vercel.app/',
@@ -15,8 +18,9 @@ export const portfolioWebData = [
     // LACEYS:
     {
         header: "The Laceys Farm Shop",
-        description: "About the site ",
+        description: "Laceys Farm Shop website has been made modern and responsive to be accessible on all devices. Colour theme and font were more consistent. More visual content was added in form of photos and collages. New look resulted in more customers being able to reach the Laceys Farm Shop and see new offers or sales.",
         src:"/Media/screenshots/laceys-site.jpg",
+        recommendation: "",
         quality: 100,
         width: 900,
         height: 933,
@@ -29,6 +33,7 @@ export const portfolioWebData = [
     {
         header: "Beauty by Chanelle",
         description: "About the site ",
+        recommendation: "",
         src:"/Media/screenshots/chanelle-site.jpg",
         quality: 100,
         width: 900,
@@ -43,6 +48,7 @@ export const portfolioWebData = [
         description: "About the site ",
         src:"/Media/screenshots/menu13-site.jpg",
         quality: 100,
+        recommendation: "",
         width: 900,
         height: 614,
         alt: "screenshot Laceys website",
@@ -53,6 +59,7 @@ export const portfolioWebData = [
     {
         header: "Aquaguacamaya - Water bottling company",
         description: "About the site ",
+        recommendation: "",
         src:"/Media/screenshots/federico-site.jpg",
         quality: 100,
         width: 900,
