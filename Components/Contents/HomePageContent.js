9b9d0f9352266e717de2da2ab768223792d3cad5
Lucy de Rojas <lucyderojas@gmail.com>
2023-02-19 13:07:30 +0000

    added arnold

--- Components/Contents/HomePageContent.js
+++ Components/Contents/HomePageContent.js
@@ -1,8 +1,6 @@
 
-
-
 import LayoutContent from "./LayoutContent";
-
+import Image from "next/image";
 
 
 export default function HomePageContent () {
@@ -19,6 +17,26 @@ export default function HomePageContent () {
 I&apos;m thrilled to have you here and hope that you find everything you&apos;re looking for. My goal is to provide you with a great user experience and valuable information. If you have any questions or feedback, please don&apos;t hesitate to reach out to me. Thank you for visiting!
 
 </p>
+
+
+<p>
+    Some of the content on these pages has been helped with ChatGP as part of experiment. Yes, machines can sometimes write better content than us humans. Together we can create some awesome stuff.
+</p>
+
+
+<Image
+    src="/Media/Photos/chatAndArnold.jpg"
+    width={900}
+    height={416}
+    alt=""
+    layout="responsive"
+
+
+/>
+
+
+
+
     </LayoutContent>);
 }
 
