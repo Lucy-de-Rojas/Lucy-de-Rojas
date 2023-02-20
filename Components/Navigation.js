65834f0e59fd2b34ebcb4f7571abce0c9d3f85a4
Lucy de Rojas <lucyderojas@gmail.com>
2023-02-20 22:59:30 +0000

    added recommendation

--- Components/Navigation.js
+++ Components/Navigation.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
-
+import {useRouter} from 'next/router';
+import {useEffect} from 'react';
 
 
 
@@ -9,20 +10,30 @@ import styles from '../styles/navigation.module.css';
 
 export default function Navigation() {
 
-
-
-    const handleClick = (event)=> {
-        console.clear();
-
-
-        event.target.style.backgroundColor = 'rgb(255, 0, 0)';
+    let route = useRouter();
 
 
 
+// controlling active = highlighted menu:
+    useEffect(()=>{
+        let pathname = route.pathname;
+        pathname = pathname.replace(/[/]/gi, '');
+        console.log('pathname:>>>>',pathname);
 
-        console.log('clicked:::>', event.target.innerHTML);
 
-    }
+
+        // about page:
+        if(pathname=='about') {
+
+            // let aboutPage = document.querySelector('.navigation_about__ebGkv');
+
+            // aboutPage.styles.backgroundColor = 'red';
+        }
+
+
+
+    },[]);
+
 
 
 
@@ -40,18 +51,18 @@ export default function Navigation() {
     return (<div className={styles.navigation}>
 
             <Link href="/about"  >
-            <a className={styles.nav} id={styles.about}  onClick={handleClick}> About</a>
+            <a className={styles.nav} id={styles.about} > About</a>
              </Link>
 
 
              <Link href="/portfolio" >
-             <a className={styles.nav} onClick={handleClick}>Portfolio </a>
+             <a className={styles.nav}  id={styles.portfolio}>Portfolio </a>
              </Link>
 
 
 
              <Link href="/contact" >
-             <a className={styles.nav} onClick={handleClick}>Contact</a>
+             <a className={styles.nav} id={styles.contact}>Contact</a>
              </Link>
 
 
