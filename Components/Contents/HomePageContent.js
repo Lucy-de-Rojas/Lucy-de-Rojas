8c1382fa8fcb00c28a0e4ca8532c749a287e5eb7
Lucy de Rojas <lucyderojas@gmail.com>
2024-01-15 16:07:50 +0000

    'auto-update'

--- Components/Contents/HomePageContent.js
+++ Components/Contents/HomePageContent.js
@@ -40,7 +40,7 @@ Up for a conversation about infusing some design enchantment into your project? 
 
 {/* to remove after testing:  🟥🟥🟥*/}
 <Image
-        src="/Media/LOGOS-Clients/FIXED/PNGs/Bosch.png"
+        src="/Media/LOGOS-clients/FIXED/PNGs/TKMaxx.png"
         width={707}
         height={175}
         alt="animations design portfolio lucy de rojas"
