01f5363983ab2029478ecfca26404186929a827d
Lucy de Rojas <lucyderojas@gmail.com>
2024-12-05 18:11:11 +0000

    added google analytics

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -2,21 +2,12 @@ import Head from "next/head";
 import Script from "next/script";
 import TopHeader from "./TopHeader";
 
-import {GoogleAnalytics} from "@next/third-parties/google"
+import {GoogleAnalytics, GoogleTagManager} from "@next/third-parties/google"
 
 
 export default function Layout({children}) {
 
 
-        function handlePageLoad() {
-
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', 'G-08YXPLNCCC');
-
-        }
 
 
 
@@ -24,21 +15,10 @@ export default function Layout({children}) {
 
 
 
-        return (<div className="flex flex-col" id="topOfPage">
+        return (<html className="flex flex-col" id="topOfPage">
+                <GoogleTagManager gtmId="G-08YXPLNCCC"/>
                 <Head>
-
-
-                        <Script src="https://www.googletagmanager.com/gtag/js?id=G-08YXPLNCCC"  async onLoad={handlePageLoad}/>
-
-
-
-
-
-
                         <link rel="icon" href="/Media/IconsAndLogos/newLogoNOtail.ico" />
-
-
-
                 </Head>
 
                         <TopHeader />
@@ -54,4 +34,4 @@ export default function Layout({children}) {
 
 
 
-                </div>);}
+                </html>);}
