71a071b8cb3e88e1efbb67a6709a2e70467a986a
Lucy de Rojas <lucyderojas@gmail.com>
2024-12-05 19:22:48 +0000

    added google analytics

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -11,16 +11,23 @@ export default function Layout({children}) {
 
 
 
-
-
-
-
         return (<div className="flex flex-col" id="topOfPage">
+
+
+
                 <GoogleTagManager gtmId="G-08YXPLNCCC"/>
                 <GoogleTagManager gtmId="GTM-TKS2XTSF" />
 
 
                 <Head>
+                        <Script scr="https://www.googletagmanager.com/gtag/js?id=G-08YXPLNCCC" />
+
+                        <Script src="../googleTag.js" />
+
+
+
+
+
                         <link rel="icon" href="/Media/IconsAndLogos/newLogoNOtail.ico" />
 
 
