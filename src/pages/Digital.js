a36827b84ec45a7013ec9a4028035c9c63483696
Lucy de Rojas <lucyderojas@gmail.com>
2024-12-06 00:16:07 +0000

    google analytics

--- src/pages/Digital.js
+++ src/pages/Digital.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 import Layout from "@/Components/Layout";
 import BackToTopButton from "@/Components/backToTopButton";
+import {GoogleAnalytics, GoogleTagManager} from "@next/third-parties/google"
 
 
 
@@ -12,6 +13,8 @@ export default function Digital () {
 
 
         let imageStyling = "my-7 flex-1 drop-shadow-2xl rounded-md";
+        let GTM_ID = "G-08YXPLNCCC";
+
 
 
 
@@ -25,16 +28,22 @@ return (<Layout>
 
 
 
-
-
-
-
-
-
-
-
+  <GoogleTagManager gtmId={GTM_ID} />
     <title>
   Creative Digital Designs | Custom Solutions for Web & Social Media | High Wycombe | Buckinghamshire | Bucks</title></Head>
+
+
+
+
+  <Script id="gtm" strategy="afterInteractive">
+      {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+      `}
+    </Script>
 <h1>Digital Design</h1>
 <main className="p-14">
 
