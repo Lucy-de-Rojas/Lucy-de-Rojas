a165fc864caf53ff8a06775a6287b1c7cb671b27
Lucy de Rojas <lucyderojas@gmail.com>
2024-12-06 00:00:02 +0000

    google analytics

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -28,27 +28,12 @@ export default function Layout({children}) {
 
                         <link rel="icon" href="/Media/IconsAndLogos/newLogoNOtail.ico" />
 
-                        <GoogleAnalytics gaId={GTM_ID}/>
-
-
-
-
-
-
 
 
 
                 </Head>
 
-                <Script id="gtm" strategy="afterInteractive">
-      {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');
-      `}
-    </Script>
+                        <GoogleAnalytics gaId={GTM_ID}/>
 
                         <TopHeader />
                         {children}
