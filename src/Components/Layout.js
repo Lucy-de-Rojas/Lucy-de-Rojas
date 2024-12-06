dcf517f5c534af790395a9f20653645c002dcf77
Lucy de Rojas <lucyderojas@gmail.com>
2024-12-06 00:48:19 +0000

    google analytics

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Script from "next/script";
 import TopHeader from "./TopHeader";
 
-import {GoogleAnalytics, GoogleTagManager} from "@next/third-parties/google"
+// import {GoogleAnalytics, GoogleTagManager} from "@next/third-parties/google"
 
 
 export default function Layout({children}) {
@@ -18,10 +18,16 @@ export default function Layout({children}) {
 
 
         return (<div className="flex flex-col" id="topOfPage">
-                
-                <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TKS2XTSF"
-className="hidden invisible w-0 h-0"></iframe></noscript>
 
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DTGHE5XWP7"></script>
+
+<Script>{
+        `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)};
+        gtag('js', new Date());
+        gtag('config', 'G-DTGHE5XWP7');`
+  }
+</Script>
 
 
 
@@ -35,15 +41,6 @@ className="hidden invisible w-0 h-0"></iframe></noscript>
 
                 </Head>
 
-                <Script id="gtm" strategy="afterInteractive">
-      {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');
-      `}
-    </Script>
 
                         <TopHeader />
                         {children}
