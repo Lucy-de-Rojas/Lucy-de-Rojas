2cce3767f4e4a6681f593e8a2fe8b17b80c76dbc
Lucy de Rojas <lucyderojas@gmail.com>
2024-12-06 00:50:47 +0000

    google analytics

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -8,26 +8,24 @@ import TopHeader from "./TopHeader";
 export default function Layout({children}) {
 
 
-        // let GTM_ID = "G-08YXPLNCCC";
-        let GTM_ID = "GTM-TKS2XTSF";
-
-
-
-
-
-
 
         return (<div className="flex flex-col" id="topOfPage">
 
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-DTGHE5XWP7"></script>
+<Script async src="https://www.googletagmanager.com/gtag/js?id=G-DTGHE5XWP7" />
+
+
+
 
 <Script>{
         `window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)};
+        function gtag(){
+        dataLayer.push(arguments);}
         gtag('js', new Date());
         gtag('config', 'G-DTGHE5XWP7');`
   }
 </Script>
+
+
 
 
 
