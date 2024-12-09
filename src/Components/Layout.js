f7c2c786fe04556951e7e8e2a45439b69fdb5dee
Lucy de Rojas <lucyderojas@gmail.com>
2024-12-09 22:43:12 +0000

    added web vitals

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,36 +1,40 @@
 import Head from "next/head";
 import Script from "next/script";
 import TopHeader from "./TopHeader";
+import { useEffect } from "react";
 
 import {GoogleAnalytics, GoogleTagManager} from "@next/third-parties/google"
 
 
 export default function Layout({children}) {
 
-        let id = "GTM-TKS2XTSF";
+        let id = "G-FYW3VGK0DF";
+        useEffect(() => {
+                window.dataLayer = window.dataLayer || [];
+                function gtag() {
+                        dataLayer.push(arguments);
+                }
+
+                gtag('js', new Date());
+                gtag('config',id);
+
+
+        }, []);
 
 
 
         return (<div className="flex flex-col" id="topOfPage">
 
 
-
-
-
-
-
-
-
-
-
                 <Head>
 
-                        <link rel="icon" href="/Media/IconsAndLogos/newLogoNOtail.ico" />
-
-
-
+                     <link rel="icon" href="/Media/IconsAndLogos/newLogoNOtail.ico" />
 
                 </Head>
+
+
+                <Script src="https://www.googletagmanager.com/gtag/js?id=G-FYW3VGK0DF" />
+
 
 
                         <TopHeader />
