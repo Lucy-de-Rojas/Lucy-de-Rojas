877c92d41a337379ac75d646af2c8eab96589d6b
Lucy de Rojas <lucyderojas@gmail.com>
2024-12-09 22:34:36 +0000

    added web vitals

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,14 @@
 import "@/styles/globals.css";
 import Script from "next/script";
+import { useReportWebVitals} from 'next/web-vitals'
 
 
 export default function App({ Component, pageProps }) {
+
+  useReportWebVitals((metric) => {
+    console.clear();
+    console.log("🔴",metric);
+  });
 
 
   return <Component {...pageProps} />;
