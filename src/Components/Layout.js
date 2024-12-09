d32ceeb4d70b668bc9d5c1e0132be763da5d5fd0
Lucy de Rojas <lucyderojas@gmail.com>
2024-12-09 17:10:07 +0000

    google analytics

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -7,26 +7,16 @@ import {GoogleAnalytics, GoogleTagManager} from "@next/third-parties/google"
 
 export default function Layout({children}) {
 
-        let id = "G-DTGHE5XWP7";
+        let id = "GTM-TKS2XTSF";
 
 
 
         return (<div className="flex flex-col" id="topOfPage">
 
 
-                <GoogleTagManager gtmId={id} />
-                <GoogleAnalytics gaId={id} />
 
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-DTGHE5XWP7"></script>
 
-<script>{
-        `window.dataLayer = window.dataLayer || [];
-        function gtag(){
-        dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-DTGHE5XWP7');`
-  }
-</script>
+
 
 
 
